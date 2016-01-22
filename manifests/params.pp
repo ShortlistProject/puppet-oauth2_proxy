6c91cc342a19948d1885e0100cb453f0d403e32e
Marcin Zaremba <marcin.zaremba@allegrogroup.com>
2016-01-22 17:30:26 +0100

    oauth2 proxy module

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,18 +4,35 @@
 #
 #
 class oauth2_proxy::params {
-  $manage_user      = true
-  $user             = 'oauth2'
-  $manage_group     = true
-  $group            = $user
-  $install_root     = '/opt/oauth2_proxy'
-  $service_template = 'oauth2_proxy.service.erb'
-  $manage_service   = true
+  $upstart_service_path = '/etc/init/oauth2_proxy.conf'
+  $systemd_service_path = '/usr/lib/systemd/system/oauth2_proxy.service'
+
+  $defaults = {
+    'manage_user'      => true,
+    'user'             => 'oauth2',
+    'manage_group'     => true,
+    'group'            => 'oauth2',
+    'install_root'     => '/opt/oauth2_proxy',
+    'service_template' => $upstart_service_path,
+    'service_provider' => 'upstart',
+    'manage_service'   => true,
+  }
+
 
   # in theory, this module should work on any linux distro that uses systemd
   # but it has only been tested on el7
   case $::osfamily {
-    'RedHat': {}
+    'RedHat': {
+      if $::operatingsystemmajrelease == '7' {
+          $overrides = {
+            'service_template' => $systemd_service_path,
+            'service_provider' => 'systemd',
+          }
+      }
+    }
+    'Debian': {
+      $overrides = {}
+    }
     default: {
       fail("Module ${module_name} is not supported on operatingsystem ${::operatingsystem}")
     }
@@ -24,9 +41,20 @@ class oauth2_proxy::params {
   # bit.ly does not provide x86 builds
   case $::architecture {
     'x86_64': {}
+    'amd64':  {}
     default: {
       fail("Module ${module_name} is not supported on architecture ${::architecture}")
     }
   }
 
+  $merged = merge($defaults, $overrides)
+
+  $manage_user      = $merged['manage_user']
+  $user             = $merged['user']
+  $manage_group     = $merged['manage_group']
+  $group            = $merged['group']
+  $install_root     = $merged['install_root']
+  $service_template = $merged['service_template']
+  $service_provider = $merged['service_provider']
+  $manage_service   = $merged['manage_service']
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -2,17 +2,20 @@
 #
 # This class should be considered private.
 #
-class oauth2_proxy::service {
+class oauth2_proxy::service (
+  $service_template = $oauth2_proxy::params::service_template,
+  $service_provider = $oauth2_proxy::params::service_provider,
+) inherits oauth2_proxy::params {
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  file { '/usr/lib/systemd/system/oauth2_proxy.service':
+  file {$service_template:
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template("${module_name}/oauth2_proxy.service.erb"),
+    content => template("${module_name}/service/${service_provider}.erb"),
   } ~>
   service { 'oauth2_proxy':
     ensure => 'running',
