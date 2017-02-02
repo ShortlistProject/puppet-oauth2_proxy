956d30fdc3e0a2f8ed98f968395be48279a07cb7
Marcin Zaremba <mrcn.zrmb@gmail.com>
2017-02-02 16:04:31 +0100

    Bump to latest version

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,13 +9,13 @@ class oauth2_proxy::install {
 
   # XXX the API to this type is confusing.  It is not idempotent unless the
   # resource name matches the subdir created when unpacking the tarball.
-  $tarball = 'oauth2_proxy-2.0.1.linux-amd64.go1.4.2'
+  $tarball = 'oauth2_proxy-2.1.linux-amd64.go1.6'
   archive { $tarball:
     ensure           => present,
     src_target       => $::oauth2_proxy::install_root,
     target           => $::oauth2_proxy::install_root,
-    url              => 'https://github.com/bitly/oauth2_proxy/releases/download/v2.0.1/oauth2_proxy-2.0.1.linux-amd64.go1.4.2.tar.gz',
-    digest_string    => '950e08d52c04104f0539e6945fc42052b30c8d1b',
+    url              => 'https://github.com/bitly/oauth2_proxy/releases/download/v2.1/oauth2_proxy-2.1.linux-amd64.go1.6.tar.gz',
+    digest_string    => '7a74b361f9edda0400d02602eacd70596d85b453',
     digest_type      => 'sha1',
     user             => $::oauth2_proxy::user,
     follow_redirects => true,
